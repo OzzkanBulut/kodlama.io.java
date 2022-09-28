cab88da3f40026e9181caab2708b81c9b561dc68
OzzkanBulut <ozkanb66@gmail.com>
2022-09-28 12:46:43 +0300

    Overloading Methods

---
+++ _35_Overloading/src/DortIslem.java
@@ -0,0 +1,11 @@
+public class DortIslem {
+
+    public int topla(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int topla(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+
+    }
+}
