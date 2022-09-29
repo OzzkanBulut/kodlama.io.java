f01c7f6b742a727394614c26bd50c8ec3fba84c6
OzzkanBulut <ozkanb66@gmail.com>
2022-09-29 17:28:34 +0300

    Polymorphism

---
+++ _38_PolymorphismDemo/src/CustomerManager.java
@@ -0,0 +1,14 @@
+public class CustomerManager {
+    private BaseLogger logger;
+
+    public CustomerManager(BaseLogger logger){
+    this.logger = logger;
+
+    }
+    public void add() {
+        System.out.println("Customer is added");
+        this.logger.log("Add log");
+
+
+    }
+}
