2dfa3cf40cf12ed35078f6c58ee0587d983493e3
OzzkanBulut <ozkanb66@gmail.com>
2022-09-30 21:46:30 +0300

    exceptionHandling-tryCatch

--- _51_exceptionHandling/src/Main.java
+++ _51_exceptionHandling/src/Main.java
@@ -1,19 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        try{
-            int[] numbers = new int[]{1,2,3};
+        try {
+            int[] numbers = new int[]{1, 2, 3};
             System.out.println(numbers[3]); // hata
-        }
-        catch( Exception exception){
-            System.out.println(exception);
-        }
-        finally {
+        } catch (StringIndexOutOfBoundsException exception) {
+            System.out.println("String error logged: " + exception);
+        } catch (ArrayIndexOutOfBoundsException exception) { // hata türüne özel kod yazabilirsin
+            System.out.println("Array index error Logged: " + exception);
+        } catch (Exception exception) { // Ya da genel olarak bir hata varsa temel bi kod yazabilirsin
+            System.out.println("Error Logged: " + exception);
+        } finally {
             System.out.println("Ben her türlü çalışırım");
         }
         // veritabanına veya dosyalara bağlandığımızda işimiz bittikten
         //        sonra kapamak için finally kullanılır
-
 
 
     }
