e727f296e463443450621724a0943dc6d25ee798
OzzkanBulut <ozkanb66@gmail.com>
2022-10-06 01:13:37 +0300

    oop with n layered app

---
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/Business/ProductManager.java
@@ -0,0 +1,34 @@
+package oopWithNLayeredApp.Business;
+
+import oopWithNLayeredApp.core.logging.Logger;
+import oopWithNLayeredApp.dataAccess.HibernateProductDao;
+import oopWithNLayeredApp.dataAccess.JdbcProductDao;
+import oopWithNLayeredApp.dataAccess.ProductDao;
+import oopWithNLayeredApp.entities.Product;
+
+import java.util.List;
+
+public class ProductManager {
+    private ProductDao productDao;
+    private Logger[] loggers;
+
+    public ProductManager(ProductDao productDao,Logger[] loggers) { // Sadece interface e bağlı --> Loosely coupled
+        this.productDao = productDao;
+        this.loggers = loggers;
+    }
+
+    public void add(Product product) throws Exception { // response request pattern yapıya göre ilerde tekrar ele alınacak
+        // iş kuralları
+        if (product.getUnitPrice() < 10) {
+            throw new Exception("Ürün fiyatı 10 dan küçük olamaz");
+        }
+
+        productDao.add(product);
+
+        for(Logger logger:loggers){
+            logger.log(product.getName());
+        }
+
+
+    }
+}
